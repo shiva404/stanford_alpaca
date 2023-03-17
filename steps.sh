efbec0b2068d8a1ba3479eb6b9e026f698d9eedf
Shiva <shiva.n404@gmail.com>
2023-03-17 14:26:02 -0700

    Adding steps

---
+++ steps.sh
@@ -0,0 +1,3 @@
+pip install -r requirements.txt
+
+train.py --model_name_or_path facebook/opt-125m --data_path /Users/macbook/code/stanford_alpaca/alpaca_data.json --output_dir /Users/macbook/code/stanford_alpaca/data/output
